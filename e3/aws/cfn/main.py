f2bf1d6b133dc06dbf5b121947e4512e5b35245f
Nicolas Roche <roche@adacore.com>
2018-06-04 12:20:39 +0200

    Add ability to use S3 as proxy

--- e3/aws/cfn/main.py
+++ e3/aws/cfn/main.py
@@ -1,17 +1,23 @@
 import abc
 import logging
+import os
 import time
 
 import botocore.exceptions
 import yaml
 from e3.aws import AWSEnv
+from e3.fs import find
 from e3.main import Main
 
 
 class CFNMain(Main, metaclass=abc.ABCMeta):
     """Main to handle CloudFormation stack from command line."""
 
-    def __init__(self, regions, force_profile=None):
+    def __init__(self, regions,
+                 force_profile=None,
+                 data_dir=None,
+                 s3_bucket=None,
+                 s3_key=''):
         """Initialize main.
 
         :param regions: list of regions on which we can operate
@@ -19,6 +25,17 @@ class CFNMain(Main, metaclass=abc.ABCMeta):
         :param force_profile: if None then add ability to select a credential
             profile to use. Otherwise profile is set
         :type force_profile: str | None
+        :param data_dir: directory containing files used by cfn-init
+        :type data_dir: str | None
+        :param s3_bucket: if defined S3 will be used as a proxy for resources.
+            Template body will be uploaded to S3 before calling operation on
+            it. This change the body limit from 50Ko to 500Ko. Additionally if
+            data_dir is defined, the directory will be uploaded to the
+            specified S3 bucket.
+        :param s3_key: if s3_bucket is defined, then all uploaded resources
+            will be stored under a subkey of s3_key. If not defined the root
+            of the bucket is used.
+        :type: str
         """
         super(CFNMain, self).__init__(platform_args=False)
         self.argument_parser.add_argument(
@@ -48,6 +65,25 @@ class CFNMain(Main, metaclass=abc.ABCMeta):
 
         self.regions = regions
 
+        self.data_dir = data_dir
+        self.s3_bucket = s3_bucket
+        self.s3_data_key = None
+        self.s3_data_url = None
+        self.s3_template_key = None
+        self.s3_template_url = None
+
+        self.timestamp = str(int(time.time()))
+
+        if s3_bucket is not None:
+            s3_root_key = '/'.join([s3_key.rstrip('/'),
+                                    self.timestamp]).strip('/') + '/'
+            self.s3_data_key = s3_root_key + 'data/'
+            self.s3_data_url = 'https://%s.s3.amazonaws.com/%s' % \
+                (self.s3_bucket, self.s3_data_key)
+            self.s3_template_key = s3_root_key + 'template'
+            self.s3_template_url = 'https://%s.s3.amazonaws.com/%s' % \
+                (self.s3_bucket, self.s3_template_key)
+
     def execute(self, args=None, known_args_only=False):
         """Execute application and return exit status.
 
@@ -60,15 +96,39 @@ class CFNMain(Main, metaclass=abc.ABCMeta):
 
         try:
             if self.args.command == 'push':
+                if self.data_dir is not None and self.s3_data_key is not None:
+                    s3 = self.aws_env.client('s3')
+
+                    # synchronize data to the bucket before creating the stack
+                    for f in find(self.data_dir):
+                        with open(f, 'rb') as fd:
+                            subkey = os.path.relpath(
+                                f,
+                                self.data_dir).replace('\\', '/')
+                            logging.info('Upload %s to %s:%s%s',
+                                         subkey, self.s3_bucket,
+                                         self.s3_data_key, subkey)
+                            s3.put_object(Bucket=self.s3_bucket,
+                                          Body=fd,
+                                          Key=self.s3_data_key + subkey)
+
                 s = self.create_stack()
 
+                if self.s3_template_key is not None:
+                    logging.info('Upload template to %s:%s',
+                                 self.s3_bucket, self.s3_template_key)
+                    s3.put_object(Bucket=self.s3_bucket,
+                                  Body=s.body.encode('utf-8'),
+                                  Key=self.s3_template_key)
+
                 logging.info('Validate template for stack %s' % s.name)
-                s.validate()
+                s.validate(url=self.s3_template_url)
 
                 if s.exists():
                     changeset_name = 'changeset%s' % int(time.time())
                     logging.info('Push changeset: %s' % changeset_name)
-                    s.create_change_set(changeset_name)
+                    s.create_change_set(changeset_name,
+                                        url=self.s3_template_url)
                     result = s.describe_change_set(changeset_name)
                     while result['Status'] in ('CREATE_PENDING',
                                                'CREATE_IN_PROGRESS'):
@@ -84,7 +144,7 @@ class CFNMain(Main, metaclass=abc.ABCMeta):
                         return 0
                 else:
                     logging.info('Create new stack')
-                    s.create()
+                    s.create(url=self.s3_template_url)
                     state = s.state()
                     if self.args.wait:
                         while 'PROGRESS' in state['Stacks'][0]['StackStatus']:
