ef9d6efa258f04e035977bc36fa59efc7593ffe7
Olivier Ramonat <ramonat@adacore.com>
2020-09-28 11:46:14 +0200

    Enable access to GitHub git repositories
    Use api.github.com/meta to retrieve the list of ip ranges used
    by GitHub to provide git access via ssh.
    
    TN: T921-018

--- src/e3/aws/cfn/arch/security.py
+++ src/e3/aws/cfn/arch/security.py
@@ -48,7 +48,7 @@ def amazon_security_groups(name, vpc):
     i = 0
     limit = 50
     sg_name = name + str(i)
-    sg = SecurityGroup(sg_name, vpc, description="Allow acces to amazon services")
+    sg = SecurityGroup(sg_name, vpc, description="Allow access to amazon services")
     sgs[sg_name] = sg
     for ip_range in amazon_ip_ranges:
         if len(sg.egress + sg.ingress) == limit:
@@ -59,4 +59,38 @@ def amazon_security_groups(name, vpc):
             )
             sgs[sg_name] = sg
         sg.add_rule(Ipv4EgressRule("https", ip_range))
+    return sgs
+
+
+def github_security_groups(name, vpc, protocol):
+    """Create a dict of security group authorizing access to github services.
+
+    As the number of rules per security group is limited to 50,
+    we create blocks of 50 rules.
+
+    :param vpc: vpc in which to create the group
+    :type vpc: VPC
+    :param protocol: protocol to allow (https, ssh)
+    :type protocol: str
+    :return: a dict of security groups indexed by name
+    :rtype: dict(str, SecurityGroup)
+    """
+    ip_ranges = requests.get("https://api.github.com/meta").json()["git"]
+
+    # Authorize ssh on the resulting list of ip ranges
+    sgs = {}
+    i = 0
+    limit = 50
+    sg_name = name + str(i)
+    sg = SecurityGroup(sg_name, vpc, description="Allow access to github")
+    sgs[sg_name] = sg
+    for ip_range in ip_ranges:
+        if len(sg.egress + sg.ingress) == limit:
+            i += 1
+            sg_name = name + str(i)
+            sg = SecurityGroup(
+                sg_name, vpc, description=f"Allow access to GitHub {protocol}"
+            )
+            sgs[sg_name] = sg
+        sg.add_rule(Ipv4EgressRule(protocol, ip_range))
     return sgs
