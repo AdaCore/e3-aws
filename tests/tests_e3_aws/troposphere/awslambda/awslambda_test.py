a942766e3d60c7534198f5c4c33430a3b3a179fb
Jérémy Morosi <morosi@adacore.com>
2023-01-16 15:26:51 +0100

    Improve awslambda coverage
    Add the missing memory_size, ephemeral_storage_size and
    logs_retention_in_days in awslambda_test.py.
    Add a test for cfn_policy_document function.

--- tests/tests_e3_aws/troposphere/awslambda/awslambda_test.py
+++ tests/tests_e3_aws/troposphere/awslambda/awslambda_test.py
@@ -29,7 +29,7 @@ EXPECTED_PY38FUNCTION_TEMPLATE = {
     }
 }
 
-EXPECTED_PYFUNCTION_TEMPLATE = {
+EXPECTED_PYFUNCTION_DEFAULT_TEMPLATE = {
     "Mypylambda": {
         "Properties": {
             "Code": {
@@ -58,6 +58,56 @@ EXPECTED_PYFUNCTION_TEMPLATE = {
     },
 }
 
+EXPECTED_PYFUNCTION_TEMPLATE = {
+    "Mypylambda": {
+        "Properties": {
+            "Code": {
+                "S3Bucket": "cfn_bucket",
+                "S3Key": "templates/mypylambda_lambda.zip",
+            },
+            "Description": "this is a test",
+            "FunctionName": "mypylambda",
+            "Handler": "app.main",
+            "Role": "somearn",
+            "Runtime": "python3.9",
+            "Timeout": 3,
+            "MemorySize": 128,
+            "EphemeralStorage": {"Size": 1024},
+            "Environment": {
+                "Variables": {"env_key_1": "env_value_1", "env_key_2": "env_value2"}
+            },
+        },
+        "Type": "AWS::Lambda::Function",
+    },
+    "MypylambdaLogGroup": {
+        "DeletionPolicy": "Retain",
+        "Properties": {
+            "LogGroupName": "/aws/lambda/mypylambda",
+            "RetentionInDays": 7,
+        },
+        "Type": "AWS::Logs::LogGroup",
+    },
+}
+
+
+EXPECTED_PYFUNCTION_POLICY_DOCUMENT = {
+    "Statement": [
+        {
+            "Action": [
+                "lambda:CreateFunction",
+                "lambda:GetFunction",
+                "lambda:DeleteFunction",
+                "lambda:UpdateFunctionCode",
+                "lambda:UpdateFunctionConfiguration",
+                "lambda:GetFunctionConfiguration",
+            ],
+            "Effect": "Allow",
+            "Resource": "arn:aws:lambda:::function:mypylambda*",
+        },
+        {"Action": ["iam:PassRole"], "Effect": "Allow", "Resource": "somearn"},
+    ],
+    "Version": "2012-10-17",
+}
 
 EXPECTED_DOCKER_FUNCTION = {
     "Dockerfunction": {
@@ -98,8 +148,8 @@ def test_py38function(stack: Stack) -> None:
     assert stack.export()["Resources"] == EXPECTED_PY38FUNCTION_TEMPLATE
 
 
-def test_pyfunction(stack: Stack) -> None:
-    """Test PyFunction creation."""
+def test_pyfunction_default(stack: Stack) -> None:
+    """Test PyFunction creation with default settings."""
     stack.s3_bucket = "cfn_bucket"
     stack.s3_key = "templates/"
     stack.add(
@@ -114,7 +164,47 @@ def test_pyfunction(stack: Stack) -> None:
         )
     )
     print(stack.export()["Resources"])
+    assert stack.export()["Resources"] == EXPECTED_PYFUNCTION_DEFAULT_TEMPLATE
+
+
+def test_pyfunction(stack: Stack) -> None:
+    """Test PyFunction creation."""
+    stack.s3_bucket = "cfn_bucket"
+    stack.s3_key = "templates/"
+    stack.add(
+        PyFunction(
+            name="mypylambda",
+            description="this is a test",
+            role="somearn",
+            runtime="python3.9",
+            code_dir="my_code_dir",
+            handler="app.main",
+            memory_size=128,
+            ephemeral_storage_size=1024,
+            logs_retention_in_days=7,
+            environment={"env_key_1": "env_value_1", "env_key_2": "env_value2"},
+        )
+    )
+    print(stack.export()["Resources"])
     assert stack.export()["Resources"] == EXPECTED_PYFUNCTION_TEMPLATE
+
+
+def test_pyfunction_policy_document(stack: Stack) -> None:
+    """Test cfn_policy_document of PyFunction."""
+    stack.s3_bucket = "cfn_bucket"
+    stack.s3_key = "templates/"
+    stack.add(
+        PyFunction(
+            name="mypylambda",
+            description="this is a test",
+            role="somearn",
+            runtime="python3.9",
+            code_dir="my_code_dir",
+            handler="app.main",
+        )
+    )
+    print(stack.cfn_policy_document().as_dict)
+    assert stack.cfn_policy_document().as_dict == EXPECTED_PYFUNCTION_POLICY_DOCUMENT
 
 
 def test_docker_function(stack: Stack) -> None:
