4078129ca0f5f78cfa1e0f80b42580737f4be9e5
Jérémy Morosi <morosi@adacore.com>
2025-12-01 12:47:50 +0100

    Add AssetLayout enum
    This will allow to choose between having S3 objects uploaded under
    `assets/AssetName/` so they are grouped by asset name, or `assets/`
    so they are all in the same place

--- tests/tests_e3_aws/troposphere/assets/assets_test.py
+++ tests/tests_e3_aws/troposphere/assets/assets_test.py
@@ -1,160 +1,132 @@
 from __future__ import annotations
-
-from copy import deepcopy
 from pathlib import Path
+import pytest
+
 from e3.aws.troposphere import Stack
-from e3.aws.troposphere.asset import FileAsset, DirectoryAsset
+from e3.aws.troposphere.asset import FileAsset, DirectoryAsset, AssetLayout
 
 CONFIG_DIR = Path(__file__).parent / "example"
 
 ASSET_URI_PREFIX = "s3://cfn_bucket/assets/"
 
-EXPECTED_FILE_ASSET_OUTPUT = {
-    "MyFileAssetS3URIOutput": {
-        "Description": "S3 URI for the Asset MyFileAsset",
-        "Export": {
-            "Name": "MyFileAssetS3URIOutput",
-        },
-        "Value": f"{ASSET_URI_PREFIX}MyFileAsset/config_file.yaml",
-    }
-}
 
-EXPECTED_FILE_ASSET_PARAMETER = {
-    "MyFileAssetS3Key": {
-        "Default": "MyFileAsset/config_file.yaml",
-        "Description": "S3 key of asset MyFileAsset",
-        "Type": "String",
-    },
-}
-
-EXPECTED_FILE_ASSET_VERSIONED = (
-    "MyFileAsset/"
-    "config_file_e39a9030d7761c2d3a6672bfbe6de80cead90dcbeb595d6d4f84cc382c799d8b.yaml"
+@pytest.mark.parametrize(
+    "versioning, layout, expected_s3_key",
+    [
+        # Without versioning
+        (False, AssetLayout.TREE, "MyFileAsset/config_file.yaml"),
+        # With versioning
+        (
+            True,
+            AssetLayout.TREE,
+            "MyFileAsset/config_file_e39a9030d7761c2d3a6672bfbe6de80cead90dcbeb"
+            "595d6d4f84cc382c799d8b.yaml",
+        ),
+        # With flat layout
+        (False, AssetLayout.FLAT, "config_file.yaml"),
+    ],
 )
+def test_file_asset(
+    versioning: bool,
+    layout: AssetLayout,
+    expected_s3_key: str,
+    stack: Stack,
+) -> None:
+    """Test FileAsset creation.
 
-EXPECTED_DIRECTORY_ASSET_OUTPUT = {
-    "MyDirectoryAssetS3URIOutput": {
-        "Description": "S3 URI for the Asset MyDirectoryAsset",
-        "Export": {
-            "Name": "MyDirectoryAssetS3URIOutput",
-        },
-        "Value": f"{ASSET_URI_PREFIX}MyDirectoryAsset/asset_dir",
-    }
-}
-
-EXPECTED_DIRECTORY_ASSET_PARAMETER = {
-    "MyDirectoryAssetS3Key": {
-        "Default": "MyDirectoryAsset/asset_dir",
-        "Description": "S3 key of asset MyDirectoryAsset",
-        "Type": "String",
-    },
-}
-
-EXPECTED_DIRECTORY_ASSET_VERSIONED = (
-    "MyDirectoryAsset/"
-    "asset_dir_231eef51fa7ec9ea25faa21441304c9360678e1ead08314ee5c6f32f04b4fad1"
-)
-
-EXPECTED_DIRECTORY_ASSET_IGNORED = (
-    "MyDirectoryAsset/"
-    "asset_dir_2fd1c0d3b3459469dc582c985d8f22da29378e373596f11edff36c80478c01e6"
-)
-
-
-def test_file_asset(stack: Stack) -> None:
-    """Test FileAsset creation."""
+    :param versioning: enable versioning or not
+    :param layout: the asset layout
+    :param expected_s3_key: expected S3 key of the asset
+    :param stack: a stack instance
+    """
     stack.add(
         FileAsset(
             name="MyFileAsset",
-            versioning=False,
+            versioning=versioning,
+            layout=layout,
             file_path=str(CONFIG_DIR / "config_file.yaml"),
         )
     )
-    assert stack.export()["Outputs"] == EXPECTED_FILE_ASSET_OUTPUT
-    assert stack.export()["Parameters"] == EXPECTED_FILE_ASSET_PARAMETER
+    assert stack.export()["Parameters"] == {
+        "MyFileAssetS3Key": {
+            "Default": expected_s3_key,
+            "Description": "S3 key of asset MyFileAsset",
+            "Type": "String",
+        },
+    }
+    assert stack.export()["Outputs"] == {
+        "MyFileAssetS3URIOutput": {
+            "Description": "S3 URI for the Asset MyFileAsset",
+            "Export": {
+                "Name": "MyFileAssetS3URIOutput",
+            },
+            "Value": f"{ASSET_URI_PREFIX}{expected_s3_key}",
+        }
+    }
 
 
-def test_file_asset_with_versioning(stack: Stack) -> None:
-    """Test FileAsset creation with versioning."""
-    stack.add(
-        FileAsset(
-            name="MyFileAsset",
-            versioning=True,
-            file_path=str(CONFIG_DIR / "config_file.yaml"),
-        )
-    )
+@pytest.mark.parametrize(
+    "versioning, ignore, layout, expected_s3_key",
+    [
+        # Without versioning
+        (False, None, AssetLayout.TREE, "MyDirectoryAsset/asset_dir"),
+        # With versioning
+        (
+            True,
+            None,
+            AssetLayout.TREE,
+            "MyDirectoryAsset/asset_dir_231eef51fa7ec9ea25faa21441304c9360678e1"
+            "ead08314ee5c6f32f04b4fad1",
+        ),
+        # With versioning and ignore some elements
+        (
+            True,
+            ["**/*_file2.yaml", "file_not_exist.txt", "to_ignore"],
+            AssetLayout.TREE,
+            "MyDirectoryAsset/asset_dir_2fd1c0d3b3459469dc582c985d8f22da29378e3"
+            "73596f11edff36c80478c01e6",
+        ),
+        # With flat layout
+        (False, None, AssetLayout.FLAT, "asset_dir"),
+    ],
+)
+def test_directory_asset(
+    versioning: bool,
+    ignore: list[str] | None,
+    layout: AssetLayout,
+    expected_s3_key: str,
+    stack: Stack,
+) -> None:
+    """Test DirectoryAsset creation.
 
-    expected_asset_output = dict(EXPECTED_FILE_ASSET_OUTPUT)
-    expected_asset_output["MyFileAssetS3URIOutput"][
-        "Value"
-    ] = f"{ASSET_URI_PREFIX}{EXPECTED_FILE_ASSET_VERSIONED}"
-
-    expected_asset_parameter = dict(EXPECTED_FILE_ASSET_PARAMETER)
-    expected_asset_parameter["MyFileAssetS3Key"][
-        "Default"
-    ] = EXPECTED_FILE_ASSET_VERSIONED
-
-    assert stack.export()["Outputs"] == expected_asset_output
-    assert stack.export()["Parameters"] == expected_asset_parameter
-
-
-def test_directory_asset(stack: Stack) -> None:
-    """Test DirectoryAsset creation."""
-    stack.add(
-        DirectoryAsset(
-            name="MyDirectoryAsset",
-            versioning=False,
-            data_dir=str(CONFIG_DIR / "asset_dir"),
-        )
-    )
-    assert stack.export()["Outputs"] == EXPECTED_DIRECTORY_ASSET_OUTPUT
-    assert stack.export()["Parameters"] == EXPECTED_DIRECTORY_ASSET_PARAMETER
-
-
-def test_directory_asset_with_versioning(stack: Stack) -> None:
-    """Test DirectoryAsset creation with versioning."""
-    stack.add(
-        DirectoryAsset(
-            name="MyDirectoryAsset",
-            versioning=True,
-            data_dir=str(CONFIG_DIR / "asset_dir"),
-        )
-    )
-
-    expected_asset_output = deepcopy(EXPECTED_DIRECTORY_ASSET_OUTPUT)
-    expected_asset_output["MyDirectoryAssetS3URIOutput"][
-        "Value"
-    ] = f"{ASSET_URI_PREFIX}{EXPECTED_DIRECTORY_ASSET_VERSIONED}"
-
-    expected_asset_parameter = deepcopy(EXPECTED_DIRECTORY_ASSET_PARAMETER)
-    expected_asset_parameter["MyDirectoryAssetS3Key"][
-        "Default"
-    ] = EXPECTED_DIRECTORY_ASSET_VERSIONED
-
-    assert stack.export()["Outputs"] == expected_asset_output
-    assert stack.export()["Parameters"] == expected_asset_parameter
-
-
-def test_directory_asset_with_ignore(stack: Stack) -> None:
-    """Test DirectoryAsset creation and ignore some elements."""
+    :param versioning: enable versioning or not
+    :param ignore: glob pattern or list of files or directories to ignore
+    :param layout: the asset layout
+    :param expected_s3_key: expected S3 key of the asset
+    :param stack: a stack instance
+    """
     stack.add(
         DirectoryAsset(
             name="MyDirectoryAsset",
-            versioning=True,
+            versioning=versioning,
+            ignore=ignore,
+            layout=layout,
             data_dir=str(CONFIG_DIR / "asset_dir"),
-            ignore=["**/*_file2.yaml", "file_not_exist.txt", "to_ignore"],
         )
     )
-
-    expected_asset_output = deepcopy(EXPECTED_DIRECTORY_ASSET_OUTPUT)
-    expected_asset_output["MyDirectoryAssetS3URIOutput"][
-        "Value"
-    ] = f"{ASSET_URI_PREFIX}{EXPECTED_DIRECTORY_ASSET_IGNORED}"
-
-    expected_asset_parameter = deepcopy(EXPECTED_DIRECTORY_ASSET_PARAMETER)
-    expected_asset_parameter["MyDirectoryAssetS3Key"][
-        "Default"
-    ] = EXPECTED_DIRECTORY_ASSET_IGNORED
-
-    assert stack.export()["Outputs"] == expected_asset_output
-    assert stack.export()["Parameters"] == expected_asset_parameter
+    assert stack.export()["Parameters"] == {
+        "MyDirectoryAssetS3Key": {
+            "Default": expected_s3_key,
+            "Description": "S3 key of asset MyDirectoryAsset",
+            "Type": "String",
+        },
+    }
+    assert stack.export()["Outputs"] == {
+        "MyDirectoryAssetS3URIOutput": {
+            "Description": "S3 URI for the Asset MyDirectoryAsset",
+            "Export": {
+                "Name": "MyDirectoryAssetS3URIOutput",
+            },
+            "Value": f"{ASSET_URI_PREFIX}{expected_s3_key}",
+        }
+    }
